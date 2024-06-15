476ea013ab9b3af6a13436dca21ed03bacc1b7d6
DarshanKrishna-DK <darshankrishna2k2@gmail.com>
2024-06-15 14:28:24 +0530

    File Structure Cleaned

--- logic.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('scrollUpBtn').addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Smooth scrolling effect
-    });
-});
