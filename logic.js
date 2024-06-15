7b88f6ecc6b20d9582d10ded40bb323746ee445c
Joyprakash Kalita <143203463+joyprakashk@users.noreply.github.com>
2024-06-15 09:52:27 +0530

    changes

---
+++ logic.js
@@ -0,0 +1,6 @@
+document.getElementById('scrollUpBtn').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scrolling effect
+    });
+});
